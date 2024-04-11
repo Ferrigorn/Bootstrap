bc302f005797e4e7cbcf3f735381264a9cec5f06
Ferrigorn <fbabures@gmail.com>
2024-04-11 16:15:39 +0200

    calculadora sin maquetar

--- Calculadora/app.js
+++ Calculadora/app.js
@@ -1,10 +1,13 @@
-//Preguntar la sintaxis de l'operació factura*propina/100
-//Alert funciona així o s'hauria de crear una const amb addEventListener pel click al botó Calcular¿?
+
 
 function calcular() {
-    let factura = document.getElementById("factura").value
-    let propina = document.getElementById("propina").value
-    let resultado = factura * propina / 100
-    document.getElementById("resultado").innerHTML = resultado
-    alert("Total a pagar =" + [factura+resultado])
+    let factura = document.getElementById("factura").value;
+    let propina = document.getElementById("propina").value;
+    let facturaint = parseInt(factura);
+    let propinaint = parseInt(propina);
+    let resultado = facturaint * propinaint / 100;
+    let resultadoint = parseInt(resultado);
+    document.getElementById("resultado").innerHTML = resultadoint;
+    let total = facturaint + resultadoint;
+    alert( total)
 }
